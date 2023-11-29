70fd5c2e4652b5efe15fd1d7d0bb214b5f823e04
mortadhaksontini <mortadhaksontini22@gmail.com>
2023-11-29 10:21:25 +0100

    adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PlayersList from "./PlayersList";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <main style={{}}>
+
+    <div className="App" >
+      <h1 style={{color: '#fff',marginTop:'15px',fontSize:'50px'}}>FIFA Player Cards</h1>
+      <PlayersList />
     </div>
+    </main>
+    </>
   );
 }
 
---
+++ src/Player.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+const Player = ({ name, team, nationality, jerseyNumber, age,image, imageUrl }) => {
+  return (
+    <>
+    <Row xs={1} md={2} className="g-4">
+      {Array.from({ length: 1 }).map((_, idx) => (
+        <Col key={idx}>
+          <Card style={{ backgroundColor:'transparent',backgroundImage: "url('https://pbs.twimg.com/media/FXrax5-UUAEDJiA.png')",backgroundRepeat: 'no-repeat',backgroundSize: 'cover',backgroundPosition: 'center',justifyContent:'space-around',color: '#806C00',border:'0' ,width: '18rem',height:'500px' ,marginRight:'15px',marginLeft:'22px'}}>
+            <Card.Img variant="top" src={imageUrl} alt={name} style={{width:'180px',marginTop:'80px',marginLeft:'55px'}}/>
+            <Card.Body>
+                <Card.Title style={{textAlign:'center',borderBottom: '1px solid #806C00',fontWeight:'700',fontFamily:'Oswald',fontSize:'23px'}}>{name}</Card.Title>
+                <Card.Text style={{marginLeft:'25px'}}>
+                <label style={{fontWeight:'500'}}>Nationality:</label> 
+                <Card.Img variant="top" src={image} alt={name} style={{width:'25px', marginLeft:'8px',borderRadius:'0',marginBottom:'3px'}}/><br />
+                <label style={{fontWeight:'500'}}>Team:</label> {team} <br />
+                <label style={{fontWeight:'500'}}>Jersey Number:</label> {jerseyNumber}<br />
+                <label style={{fontWeight:'500'}}>Age:</label> {age}<br />
+                </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+    
+    </>
+  );
+};
+
+export default Player;
